706defaf30f9be9cb06f58090752c267617643a0
Ramin Ahadi <61704325+rahadi94@users.noreply.github.com>
2023-06-08 09:58:37 +0200

    Adding simpy examples

---
+++ 08/ChargingEV.py
@@ -0,0 +1,27 @@
+import simpy
+
+env = simpy.Environment()
+bcs = simpy.Resource(env, capacity=2)
+
+
+def car(env, name, bcs, driving_time, charge_duration):
+    # Simulate driving to the BCS
+    yield env.timeout(driving_time)
+
+    # Request one of its charging spots
+    print('%s arriving at %d' % (name, env.now))
+    with bcs.request() as req:
+        yield req
+
+        # Charge the battery
+        print('%s starting to charge at %s' % (name, env.now))
+        yield env.timeout(charge_duration)
+        print('%s leaving the bcs at %s' % (name, env.now))
+
+
+for i in range(20):
+    env.process(car(env, 'Car %d' % i, bcs, i * 2, 5))
+
+
+env.run()
+
